876085d7eb63f8f396c84da02b1fc08a4ffe89b6
badjasonbourne <145309560+csbloodhound@users.noreply.github.com>
2023-11-16 10:10:44 +0800

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,10 @@
-// config for github pages
-
-import adapter from '@sveltejs/adapter-static';
-
-const dev = process.argv.includes('dev');
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-		paths: {
-			// base: dev ? '' : '/data-driven-breakpoints'
-		}
-	}
+    kit: {
+        adapter: adapter()
+    }
 };
 
 export default config;
